13a3f38538574a66e1b361dbe13626affd05b325
princelewis <princelewis4christ@yahoo.com>
2019-05-29 02:25:50 +0100

    solve the word length problem

--- WordLength/WordLengthSolution.js
+++ WordLength/WordLengthSolution.js
@@ -1,5 +1,11 @@
 function wordLength(arr) {
-
+    var length = [];
+    var word = [];
+    var myMap = new Map();
+    for (var i of arr) {
+        myMap.set(i.toLowerCase(),i.length);
+    }
+    return myMap;
 }
 
 module.exports = wordLength;
